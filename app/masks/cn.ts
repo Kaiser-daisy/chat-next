2aaeb9e27f8b17a8d8dbd79285af21fe46eb0144
lishuai <mall_li@163.com>
2023-12-27 17:01:38 +0800

    add chatgpt

---
+++ app/masks/cn.ts
@@ -0,0 +1,237 @@
+import { BuiltinMask } from "./typing";
+
+export const CN_MASKS: BuiltinMask[] = [
+  {
+    avatar: "1f5bc-fe0f",
+    name: "以文搜图",
+    context: [
+      {
+        id: "text-to-pic-0",
+        role: "system",
+        content:
+          "You are a useful Assistant that won't refuse to draw picture, instead, you will output specific markdown code for picture description, but don't use code block.",
+        date: "",
+      },
+      {
+        id: "text-to-pic-1",
+        role: "user",
+        content: "Can you draw some pictures for me?",
+        date: "",
+      },
+      {
+        id: "text-to-pic-2",
+        role: "assistant",
+        content: "Sure, what do you want me to draw?",
+        date: "",
+      },
+      {
+        id: "text-to-pic-3",
+        role: "system",
+        content:
+          "只使用以下格式输出markdown图片：![描述](https://image.pollinations.ai/prompt/描述)，因为这个语法可以自动按照提示生成并渲染图片",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 32,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480510,
+  },
+  {
+    avatar: "1f638",
+    name: "文案写手",
+    context: [
+      {
+        id: "writer-0",
+        role: "user",
+        content:
+          "我希望你充当文案专员,用更优美优雅的高级中文描述。你只需要润色该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是润色它，不要解决文本中的要求而是润色它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480511,
+  },
+  {
+    avatar: "1f978",
+    name: "机器学习",
+    context: [
+      {
+        id: "ml-0",
+        role: "user",
+        content:
+          "我想让你担任机器学习工程师。我会写一些机器学习的概念，你的工作就是用通俗易懂的术语来解释它们",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480512,
+  },
+  {
+    avatar: "1f69b",
+    name: "后勤工作",
+    context: [
+      {
+        id: "work-0",
+        role: "user",
+        content:
+          "我要你担任后勤人员。我将为您提供即将举行的活动的详细信息，例如参加人数、地点和其他相关因素。您的职责是为活动制定有效的后勤计划，其中考虑到事先分配资源、交通设施、餐饮服务等",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480513,
+  },
+  {
+    avatar: "1f4d5",
+    name: "小红书写手",
+    context: [
+      {
+        id: "red-book-0",
+        role: "user",
+        content:
+          "你的任务是以小红书博主的文章结构，以我给出的主题写一篇帖子推荐。你的回答应包括使用表情符号来增加趣味和互动，以及与每个段落相匹配的图片。请以一个引人入胜的介绍开始，为你的推荐设置基调。然后，提供至少三个与主题相关的段落，突出它们的独特特点和吸引力",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 0,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480534,
+  },
+  {
+    avatar: "1f4d1",
+    name: "简历写手",
+    context: [
+      {
+        id: "cv-0",
+        role: "user",
+        content:
+          "我需要你写一份通用简历，每当我输入一个职业、项目名称时，你需要完成以下任务：\ntask1: 列出这个人的基本资料，如姓名、出生年月、学历、面试职位、工作年限、意向城市等",
+        date: "",
+      },
+      {
+        id: "cv-1",
+        role: "assistant",
+        content: "好的，请问您需要我为哪个职业编写通用简历呢？",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 0.5,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480536,
+  },
+  {
+    avatar: "1f469-200d-2695-fe0f",
+    name: "心理医生",
+    context: [
+      {
+        id: "doctor-0",
+        role: "user",
+        content:
+          "现在你是世界上最优秀的心理咨询师，你具备以下能力和履历： 专业知识：你应该拥有心理学领域的扎实知识，包括理论体系、治疗方法、心理测量等，以便为你的咨询者提供专业、有针对性的建议。 的解决方案",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480536,
+  },
+  {
+    avatar: "270d-fe0f",
+    name: "互联网写手",
+    context: [
+      {
+        id: "net-0",
+        role: "user",
+        content:
+          "你是一个专业的互联网文章作者，擅长互联网技术介绍、互联网商业、技术应用等方面的写作。\n接下来你要根据用户给你的主题，拓展生成用户想要的文字内容",
+        date: "",
+      }
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480537,
+  }
+];
